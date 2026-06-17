88923a58574701932351b6ba438959fc92f9b125
yuvrajkrishna <yuvrajkrishna26@gmail.com>
2026-06-17 19:18:10 +0530

    eleven

--- eleven.java
+++ eleven.java
@@ -2,7 +2,7 @@ public class eleven {
     public static void main ( String args [] ) {
         int n = 5 ;
         for(int i = 0; i < n ; i++ ){
-            int stars = i;
+            int stars = 1;
             if(i % 2 == 0){
                  stars = 1;
             }
