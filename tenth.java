06463d93eb46469259ec75f70fabafef684ed221
yuvrajkrishna <yuvrajkrishna26@gmail.com>
2026-06-17 17:01:15 +0530

    tenth

---
+++ tenth.java
@@ -0,0 +1,20 @@
+public class tenth {
+    public static void main(String args[]){
+
+        int n = 5; 
+        
+        for ( int i = 1 ; i <= 2*n - 1 ; i++ ) {
+            int stars = i;
+            if(i> n){
+                stars = 2*n - i;
+            }
+            for ( int j = 0 ; j < stars; j++ ){
+                
+                System.out.print("*");
+            }
+            System.out.println();
+
+        }
+
+    }
+}
