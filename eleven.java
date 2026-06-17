4e7179d7918df75a64770e62138e91b7a56d6279
yuvrajkrishna <yuvrajkrishna26@gmail.com>
2026-06-17 18:02:56 +0530

    eleven

---
+++ eleven.java
@@ -0,0 +1,20 @@
+public class eleven {
+    public static void main ( String args [] ) {
+        int n = 5 ;
+        for(int i = 0; i < n ; i++ ){
+            int stars = i;
+            if(i % 2 == 0){
+                 stars = 1;
+            }
+            else{
+            stars = 0;
+            }
+            for(int j = 0 ; j <= i ; j++){
+                System.out.print(stars +  " ");
+                stars = 1 - stars;
+                
+            }
+            System.out.println();
+        }
+    }
+}
--- tenth.java
+++ tenth.java
@@ -5,7 +5,7 @@ public class tenth {
         
         for ( int i = 1 ; i <= 2*n - 1 ; i++ ) {
             int stars = i;
-            if(i> n){
+            if(i > n){
                 stars = 2*n - i;
             }
             for ( int j = 0 ; j < stars; j++ ){
