35cb45ea8ab8dac2dbba7a59da0d965d8ff6e8a8
yuvrajkrishna <yuvrajkrishna26@gmail.com>
2026-06-17 20:00:07 +0530

    true twelve

--- twelve.java
+++ twelve.java
@@ -1,28 +1,20 @@
 public class twelve {
     public static void main(String[] args) {
         int n = 5;
-        for(int i = 0;i<n;i++){
-            for(int j = 1 ; j <=i; j++){
+        int space = 2  * ( n - 1);
+        for(int i = 1 ; i <= n ; i++){
+            for(int j = 1 ; j <=i;j++){
                 System.out.print(j);
             }
-            for(int j = 0 ; j < n - i -1 ; j++){
+            for(int j = 1 ; j<=space ; j++){
                 System.out.print(" ");
             }
-            
-        
-
-        
-            for(int j = 0 ; j < n - i -1 ; j++){
-                System.out.print(" ");
-            }
-
-            for(int j = 1 ; j <=i; j++){
+            for(int j = i ; j > 0 ; j--){
                 System.out.print(j);
             }
-            
             System.out.println();
-
+            space -=2;
         }
-        
     }
 }
+
